560e49a58047802491974331641d5a3120414f00
wnr <wnr@ATEC2495>
2025-01-30 14:41:51 +0100

    CSV import

--- CSV-Test.py
+++ CSV-Test.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 30 13:57:10 2025
+import csv
 
-@author: wnr
-"""
+d= []
+with open(r"C:\tgw\visualproject\tia exporter\UDTKonverter\UDTKonverter\UDTKonverter\bin\Debug\net8.0-windows\Buffer.csv") as csvfile:
+    allezeilen = csv.reader(csvfile,delimiter=';',quotechar='"')
+    for row in allezeilen:
+        tempstring = row[0]
+        split = tempstring.split("_")
+        modtyp = split[1]
+        if int(modtyp) > 1399:
+            print(modtyp)
 
